55aa8d3f4574acb9e6704fa9a098b5bd599bfd03
yuanhuihai <yuanhuihai@sina.com>
2023-05-20 16:09:03 +0800

    20230520建立连接

--- ABBCom/Form1.Designer.cs
+++ ABBCom/Form1.Designer.cs
@@ -28,46 +28,358 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.listView1 = new System.Windows.Forms.ListView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.SystemName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.IPAddress = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Version = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.IsVirtual = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ControllerName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.scanRobot = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.PX1 = new System.Windows.Forms.TextBox();
+            this.PY1 = new System.Windows.Forms.TextBox();
+            this.PZ1 = new System.Windows.Forms.TextBox();
+            this.RX1 = new System.Windows.Forms.TextBox();
+            this.RY1 = new System.Windows.Forms.TextBox();
+            this.RZ1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.getPos = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.getStatus = new System.Windows.Forms.Button();
+            this.statusOne = new System.Windows.Forms.TextBox();
+            this.statusTwo = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // listView1
             // 
-            this.listView1.Location = new System.Drawing.Point(25, 26);
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.SystemName,
+            this.IPAddress,
+            this.Version,
+            this.IsVirtual,
+            this.ControllerName});
+            this.listView1.Location = new System.Drawing.Point(18, 20);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(674, 97);
+            this.listView1.Size = new System.Drawing.Size(862, 97);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.conRobot);
             // 
-            // button1
+            // SystemName
             // 
-            this.button1.Location = new System.Drawing.Point(25, 141);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.SystemName.Text = "系统名称";
+            this.SystemName.Width = 120;
+            // 
+            // IPAddress
+            // 
+            this.IPAddress.Text = "IP地址";
+            this.IPAddress.Width = 120;
+            // 
+            // Version
+            // 
+            this.Version.Text = "版本";
+            this.Version.Width = 120;
+            // 
+            // IsVirtual
+            // 
+            this.IsVirtual.Text = "是否虚拟机";
+            this.IsVirtual.Width = 80;
+            // 
+            // ControllerName
+            // 
+            this.ControllerName.Text = "控制器名称";
+            this.ControllerName.Width = 120;
+            // 
+            // scanRobot
+            // 
+            this.scanRobot.Location = new System.Drawing.Point(18, 123);
+            this.scanRobot.Name = "scanRobot";
+            this.scanRobot.Size = new System.Drawing.Size(75, 23);
+            this.scanRobot.TabIndex = 1;
+            this.scanRobot.Text = "扫描机器人";
+            this.scanRobot.UseVisualStyleBackColor = true;
+            this.scanRobot.Click += new System.EventHandler(this.scanRobot_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(151, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(151, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(151, 127);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "label7";
+            // 
+            // PX1
+            // 
+            this.PX1.Location = new System.Drawing.Point(96, 68);
+            this.PX1.Name = "PX1";
+            this.PX1.Size = new System.Drawing.Size(35, 21);
+            this.PX1.TabIndex = 9;
+            // 
+            // PY1
+            // 
+            this.PY1.Location = new System.Drawing.Point(96, 95);
+            this.PY1.Name = "PY1";
+            this.PY1.Size = new System.Drawing.Size(35, 21);
+            this.PY1.TabIndex = 10;
+            // 
+            // PZ1
+            // 
+            this.PZ1.Location = new System.Drawing.Point(96, 122);
+            this.PZ1.Name = "PZ1";
+            this.PZ1.Size = new System.Drawing.Size(35, 21);
+            this.PZ1.TabIndex = 11;
+            // 
+            // RX1
+            // 
+            this.RX1.Location = new System.Drawing.Point(210, 68);
+            this.RX1.Name = "RX1";
+            this.RX1.Size = new System.Drawing.Size(35, 21);
+            this.RX1.TabIndex = 12;
+            // 
+            // RY1
+            // 
+            this.RY1.Location = new System.Drawing.Point(210, 95);
+            this.RY1.Name = "RY1";
+            this.RY1.Size = new System.Drawing.Size(35, 21);
+            this.RY1.TabIndex = 13;
+            // 
+            // RZ1
+            // 
+            this.RZ1.Location = new System.Drawing.Point(210, 122);
+            this.RZ1.Name = "RZ1";
+            this.RZ1.Size = new System.Drawing.Size(35, 21);
+            this.RZ1.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(571, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "机器人当前状态";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.getPos);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.PX1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.RZ1);
+            this.groupBox1.Controls.Add(this.PY1);
+            this.groupBox1.Controls.Add(this.RY1);
+            this.groupBox1.Controls.Add(this.PZ1);
+            this.groupBox1.Controls.Add(this.RX1);
+            this.groupBox1.Location = new System.Drawing.Point(25, 186);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(290, 162);
+            this.groupBox1.TabIndex = 22;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "坐标位置";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(76, 32);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 16);
+            this.radioButton1.TabIndex = 23;
+            this.radioButton1.Text = "关节坐标";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(179, 32);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 16);
+            this.radioButton2.TabIndex = 24;
+            this.radioButton2.Text = "笛卡尔坐标";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // getPos
+            // 
+            this.getPos.Location = new System.Drawing.Point(6, 29);
+            this.getPos.Name = "getPos";
+            this.getPos.Size = new System.Drawing.Size(56, 23);
+            this.getPos.TabIndex = 25;
+            this.getPos.Text = "获取";
+            this.getPos.UseVisualStyleBackColor = true;
+            this.getPos.Click += new System.EventHandler(this.getPos_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Controls.Add(this.scanRobot);
+            this.groupBox2.Location = new System.Drawing.Point(25, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(886, 153);
+            this.groupBox2.TabIndex = 23;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "扫描机器人（双击连接）";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.statusTwo);
+            this.groupBox3.Controls.Add(this.statusOne);
+            this.groupBox3.Controls.Add(this.getStatus);
+            this.groupBox3.Location = new System.Drawing.Point(25, 365);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(290, 68);
+            this.groupBox3.TabIndex = 24;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "机器人状态";
+            // 
+            // getStatus
+            // 
+            this.getStatus.Location = new System.Drawing.Point(6, 31);
+            this.getStatus.Name = "getStatus";
+            this.getStatus.Size = new System.Drawing.Size(56, 23);
+            this.getStatus.TabIndex = 25;
+            this.getStatus.Text = "获取";
+            this.getStatus.UseVisualStyleBackColor = true;
+            this.getStatus.Click += new System.EventHandler(this.getStatus_Click);
+            // 
+            // statusOne
+            // 
+            this.statusOne.Location = new System.Drawing.Point(96, 31);
+            this.statusOne.Name = "statusOne";
+            this.statusOne.Size = new System.Drawing.Size(77, 21);
+            this.statusOne.TabIndex = 26;
+            // 
+            // statusTwo
+            // 
+            this.statusTwo.Location = new System.Drawing.Point(179, 31);
+            this.statusTwo.Name = "statusTwo";
+            this.statusTwo.Size = new System.Drawing.Size(83, 21);
+            this.statusTwo.TabIndex = 27;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.listView1);
+            this.ClientSize = new System.Drawing.Size(923, 532);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button scanRobot;
+        private System.Windows.Forms.ColumnHeader SystemName;
+        private System.Windows.Forms.ColumnHeader IPAddress;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox PX1;
+        private System.Windows.Forms.TextBox PY1;
+        private System.Windows.Forms.TextBox PZ1;
+        private System.Windows.Forms.TextBox RX1;
+        private System.Windows.Forms.TextBox RY1;
+        private System.Windows.Forms.TextBox RZ1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader Version;
+        private System.Windows.Forms.ColumnHeader IsVirtual;
+        private System.Windows.Forms.ColumnHeader ControllerName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button getPos;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox statusTwo;
+        private System.Windows.Forms.TextBox statusOne;
+        private System.Windows.Forms.Button getStatus;
     }
 }
 
--- ABBCom/Form1.cs
+++ ABBCom/Form1.cs
@@ -7,44 +7,186 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using ABB.Robotics.Controllers;
-using ABB.Robotics.Controllers.Configuration;
+using ABB.Robotics.Controllers.Discovery;
+using ABB.Robotics.Controllers.MotionDomain;
+using RobotStudio.Services.RobApi.RobApi1;
 
 namespace ABBCom
 {
     public partial class Form1 : Form
     {
+        NetworkScanner Scanner = null;
+        NetworkScanner scanner = null;
+
+        static Controller controller;
+
+
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
+     
 
         }
-        //搜素可连接的机器人
-        private void button1_Click(object sender, EventArgs e)
+
+
+        //搜素可连接的机器人 20230520
+
+        private void scanRobot_Click(object sender, EventArgs e)
         {
-          
-          
-                if (Scanner == null)
-                {
-                    scanner = new NetworkScanner();
-                }
-                scanner.Scan();
-                ControllerInfoCollection controls = scanner.Controllers;
-                this.listView1.Items.Clear();
-                foreach (ControllerInfo info in controls)
-                {
-                    ListViewItem item = new ListViewItem(info.SystemName);
-                    item.SubItems.Add(info.IPAddress.ToString());//显示IP地址
-                    item.SubItems.Add(info.Version.ToString());//显示robotware版本
-                    item.SubItems.Add(info.IsVirtual.ToString()); 显示是否为虚拟控制
-                    item.SubItems.Add(info.ControllerName.ToString());//显示控制器名称
-                    item.Tag = info;
-                    this.listView1.Items.Add(item);
-                }
-            
+
+            if (scanner == null)
+            {
+                scanner = new NetworkScanner();
+            }
+            scanner.Scan();
+            ControllerInfoCollection controls = scanner.Controllers;
+            this.listView1.Items.Clear();
+            foreach (ControllerInfo info in controls)
+            {
+                ListViewItem item = new ListViewItem(info.SystemName);
+                item.SubItems.Add(info.IPAddress.ToString());//显示IP地址
+                item.SubItems.Add(info.Version.ToString());//显示robotware版本
+                item.SubItems.Add(info.IsVirtual.ToString());// 显示是否为虚拟控制
+                item.SubItems.Add(info.ControllerName.ToString());//显示控制器名称
+                item.Tag = info;
+                this.listView1.Items.Add(item);
+            }
         }
+
+
+        //列表双击连接机器人 20230520
+        private void conRobot(object sender, EventArgs e)
+        {
+
+
+                if (this.listView1.Items.Count > 0)
+                {
+                    ListViewItem item = this.listView1.SelectedItems[0];
+                    if (item != null)
+                    {
+                        ControllerInfo info = (ControllerInfo)item.Tag;
+                        if (info.Availability == Availability.Available)
+                        {
+                            if (controller != null)
+                            {
+                                controller.Logoff();
+                                controller.Dispose();
+                                controller = null;
+                            }
+                            controller = ControllerFactory.CreateFrom(info);
+                            controller.Logon(UserInfo.DefaultUser);
+                            MessageBox.Show("已经登录控制器" + info.SystemName);
+                        currentstatus();
+                 
+                        }
+                    }
+                }
+   
+     
+
+        }
+
+
+        // 获取坐标 20230520
+        private void getPos_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        //每1s钟获取一次坐标 20230520
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //关节坐标
+            if (radioButton1.Checked)
+            {
+                ABB.Robotics.Controllers.RapidDomain.JointTarget RJ = controller.MotionSystem.ActiveMechanicalUnit.GetPosition();
+                label2.Text = "J1:";
+                label3.Text = "J2:";
+                label4.Text = "J3:";
+                label5.Text = "J4:";
+                label6.Text = "J5:";
+                label7.Text = "J6:";
+                PX1.Text = RJ.RobAx.Rax_1.ToString(format: "0.00");
+                PY1.Text = RJ.RobAx.Rax_2.ToString(format: "0.00");
+                PZ1.Text = RJ.RobAx.Rax_3.ToString(format: "0.00");
+                RX1.Text = RJ.RobAx.Rax_4.ToString(format: "0.00");
+                RY1.Text = RJ.RobAx.Rax_5.ToString(format: "0.00");
+                RZ1.Text = RJ.RobAx.Rax_6.ToString(format: "0.00");
+            }
+
+            //基坐标
+            if (radioButton2.Checked)
+            {
+                double tx;
+                double ty;
+                double tz;
+                ABB.Robotics.Controllers.RapidDomain.RobTarget robTarget = controller.MotionSystem.ActiveMechanicalUnit.GetPosition(CoordinateSystemType.World);
+                label2.Text = "PX:";
+                label3.Text = "PY:";
+                label4.Text = "PZ:";
+                label5.Text = "RX:";
+                label6.Text = "RY:";
+                label7.Text = "RZ;";
+                PX1.Text = robTarget.Trans.X.ToString(format: "0.00");
+                PY1.Text = robTarget.Trans.Y.ToString(format: "0.00");
+                PZ1.Text = robTarget.Trans.Z.ToString(format: "0.00");
+                robTarget.Rot.ToEulerAngles(out tx, out ty, out tz);
+                RX1.Text = tx.ToString(format: "0.00");
+                RY1.Text = ty.ToString(format: "0.00");
+                RZ1.Text = tz.ToString(format: "0.00");
+
+
+            }
+        }
+
+
+        //获取机器人当前状态 20230520
+
+        private void getStatus_Click(object sender, EventArgs e)
+        {
+            ControllerState con_states = new ControllerState();
+            con_states = controller.State;
+            ControllerOperatingMode op_mode = new ControllerOperatingMode();
+            op_mode = controller.OperatingMode;
+            string T1 = "";
+            string T2 = "";
+            if (con_states.Equals(ControllerState.EmergencyStop)) T1 = "急停";
+            else if (con_states.Equals(ControllerState.EmergencyStopReset)) T1 = "急停复位";
+            else if (con_states.Equals(ControllerState.GuardStop)) T1 = "安全停止";
+            else if (con_states.Equals(ControllerState.Init)) T1 = "初始化";
+            else if (con_states.Equals(ControllerState.MotorsOff)) T1 = "电机关闭";
+            else if (con_states.Equals(ControllerState.MotorsOn)) T1 = "电机启动";
+            else if (con_states.Equals(ControllerState.SystemFailure)) T1 = "系统失效";
+            else if (con_states.Equals(ControllerState.Unknown)) T1 = "未知";
+            else T1 = "严重错误";
+            if (op_mode.Equals(ControllerOperatingMode.Auto)) T2 = "自动";
+            else if (op_mode.Equals(ControllerOperatingMode.AutoChange)) T2 = "自动改";
+            else if (op_mode.Equals(ControllerOperatingMode.Init)) T2 = "初始化";
+            else if (op_mode.Equals(ControllerOperatingMode.ManualFullSpeed)) T2 = "手动全速";
+            else if (op_mode.Equals(ControllerOperatingMode.ManualFullSpeedChange)) T2 = "手动全速改";
+            else if (op_mode.Equals(ControllerOperatingMode.ManualReducedSpeed)) T2 = "手动减速";
+            else if (op_mode.Equals(ControllerOperatingMode.NotApplicable)) T2 = "不适用";
+            else T2 = "严重错误";
+
+
+            statusOne.Text = T1;
+            statusTwo.Text = T2;
+
+        }
+
+
+
+
+
+        static public double[] GetPosition(CoordinateSystemType type)//返回坐标值
+        {
+            double[] Pos = new double[7];
+            string PosStr = controller.MotionSystem.MechanicalUnits[0].GetPosition(type).ToString();
+            string[] Poses = PosStr.Split(new char[] { ' ', '{', '}', '[', ']', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < 7; i++) Pos[i] = Convert.ToDouble(Poses[i]);
+            return Pos;
+        }
+
+       
     }
 }
